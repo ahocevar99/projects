bf00fa0d86d5862829747d05d2ee61d963f3d699
ahocevar99 <andrej.hocevar11@gmail.com>
2023-02-04 21:48:03 +0100

    work on users and products

--- api.py
+++ api.py
@@ -7,63 +7,108 @@ def mainMenu():
     text = input ("\nenter the number: ")
 
     if text == "1":
-        usersFunction()
+        getData("users")
+    if text == "2":
+        getData("carts")
+    if text == "3":
+        getData("products")
     else: 
         print ("\nUnknown input.\n")
         mainMenu()
 
-def usersFunction ():
-    def findUser():
-        getId = input ("enter id of a user: ")
-        usersFound = requests.get ('https://dummyjson.com/users/{}'.format(getId))
-        if usersFound.status_code == 404: print (("User with id '{}' not found. ").format(getId)), findUser()
+def getData (resource):
+    def findById():
+        getId = input ("enter id: ")
+        dataFound = requests.get ('https://dummyjson.com/{}/{}'.format(resource, getId))
+        if dataFound.status_code == 404: print (("{} with id '{}' not found. ").format(resource[0:-1],getId)), findById()
         else: 
-            usersFound = usersFound.json()
-            print (("\nUSER with id: '{}'").format(getId))
-            for param in usersFound:
-                print ('{}: {}'.format(param, usersFound[param]))
+            dataFound = dataFound.json()
+            print (("\nDATA with id: '{}'").format(getId))
+            for param in dataFound:
+                print ('{}: {}'.format(param, dataFound[param]))
             print ("\n")
-        usersMenu()
-    def getAllUsers():
-        usersFound = requests.get ('https://dummyjson.com/users')
-        usersFound = usersFound.json()
-        if usersFound['total'] == 0:
-            print ("\nNo users found. \n")
+        dataMenu(resource)
+    def getAllData():
+        dataFound = requests.get ('https://dummyjson.com/{}'.format (resource))
+        dataFound = dataFound.json()
+        if dataFound['total'] == 0:
+            print ("\nNo data found. \n")
         else:
-            usersFound = usersFound['users']
-            print ('\nALL USERS: \n')
-            for i in usersFound:
-                print ('{}: {} {}, {}, age: {}'.format(i['id'], i['firstName'], i['lastName'], i['gender'], i['age']))
-        usersMenu()
-    def saveImageUser():
-        getId = input ("Type user's ID to save his/her image: ")
-        usersFound = requests.get('https://dummyjson.com/users/{}'.format(getId))
-        if usersFound.status_code == 404: print (("User with id '{}' not found. ").format(getId)), saveImageUser()
+            dataFound = dataFound[resource]
+            if resource == "users":
+                print ('\nALL USERS: \n')
+                for i in dataFound:
+                    print ('{}: {} {}, {}, age: {}'.format(i['id'], i['firstName'], i['lastName'], i['gender'], i['age']))
+            elif resource == "products":
+                print ('\nALL PRODUCTS: \n')
+                for i in dataFound:
+                    print ('{}: {}, {}, price: {}'.format(i['id'], i['title'], i['description'], i['price']))
+        dataMenu(resource)
+    def saveDataImage():
+        getId = input ("Enter the id: ")
+        dataFound = requests.get('https://dummyjson.com/{}/{}'.format(resource, getId))
+        if dataFound.status_code == 404: print (("{} with id '{}' not found. ").format(resource[0:-1], getId)), saveDataImage()
         else: 
-            usersFound = usersFound.json()
-            imgUrl = usersFound['image'] or ""
-            if imgUrl == "": print ("no image found"),  saveImageUser()
-            img = Image.open(requests.get(imgUrl, stream = True).raw)
-            img.save(("{} {}.png").format(usersFound['firstName'], usersFound['lastName']))
-            print ("image saved\n"), usersMenu() 
-    def searchUsersWithParams():
-        return -1
-    def usersMenu():
-        print ("\nUSERS MENU: ")
-        print ("1: find user by id")
-        print ("2: get all users")
-        print ("3: save image of an user")
-        print ("4: search users with parametres")
-        print ("5: add user")
-        print ("6: update user")
-        print ("7: delete user")
+            dataFound = dataFound.json()
+            if resource == "users": imgUrl = dataFound['image']
+            elif resource == "products": imgUrl = dataFound['images'] 
+            index = 0
+            if resource == "users":
+                img = Image.open(requests.get(imgUrl, stream = True).raw)
+                index=+1
+                img.save(("{} {}.png").format(dataFound['firstName'], dataFound['lastName']))
+            elif resource == "products":
+                for image in imgUrl:
+                    img = Image.open(requests.get(image, stream = True).raw)
+                    index += 1
+                    img.save(("{} {}.png").format(dataFound['title'], index))
+            print (("{} image(s) saved\n").format(index)), dataMenu(resource) 
+    def searchDataWithParams():
+        if resource == "users":
+            print ("\nFIND USER by KEY and VALUE\n")
+            key = input ("enter key: ")
+            value = input ("enter value: ")
+            dataFound = requests.get('https://dummyjson.com/users/filter?key={}&value={}'.format(key,value))
+            dataFound = dataFound.json()
+            dataFound = dataFound['users']
+            print ("\n{} USERS FOUND: ".format(len (dataFound)))
+            for i in dataFound:
+                print ('{} {}, {} = {}, id = {}'.format(i['firstName'], i['lastName'], key, i['{}'.format(key)], i['id']))
+            dataMenu(resource)
+        elif resource == "products":
+            print ("\nsearch by CATEGORIES: \n")
+            categories = requests.get ('https://dummyjson.com/products/categories')
+            categories = categories.json()
+            index = 0
+            for cat in categories:
+                offset = 20 - len(cat)
+                print (cat, end =" "+' '*offset)
+                index+=1
+                if index % 4==0: print ("\n")
+            category = input ("Enter a category: ")
+            dataFound = requests.get ('https://dummyjson.com/products/category/{}'. format(category))
+            dataFound = dataFound.json()
+            dataFound = dataFound['products']
+            print ("\n{} PRODUCTS FOUND: ".format(len (dataFound)))
+            for product in dataFound:
+                print ('{}, price: {}, category: {}, id = {}'.format(product['title'], product['price'], product['category'], product['id']))
+            dataMenu(resource)
+    def dataMenu(resource):
+        print ("\n{} MENU: ".format(resource.upper()))
+        print ("1: find {} by id".format(resource[0:-1]))
+        print ("2: get all {}".format(resource))
+        print ("3: save image(s) of an {}".format(resource[0:-1]))
+        print ("4: search {} with parametres".format(resource))
+        print ("5: add {}".format(resource[0:-1]))
+        print ("6: update {}".format(resource[0:-1]))
+        print ("7: delete {}".format(resource[0:-1]))
         print ("-1: back to main menu")
         inp = input ("\nenter the number: ")
-        if inp == "1": findUser()
-        elif inp == "2": getAllUsers()
-        elif inp == "3": saveImageUser()
-        elif inp == "4": searchUsersWithParams()
+        if inp == "1": findById()
+        elif inp == "2": getAllData()
+        elif inp == "3": saveDataImage()
+        elif inp == "4": searchDataWithParams()
         elif inp == "-1": print ("\n"), mainMenu()
-        else: print ("\nunknown command"), usersMenu()
-    usersMenu()   
+        else: print ("\nunknown command"), dataMenu()
+    dataMenu(resource)   
 mainMenu()
